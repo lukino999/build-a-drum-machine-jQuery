bf509fe85764c21b3d60d9f89844c943e9b02694
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-03-25 20:47:19 +0000

    Add complete html and samples

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const currentTime = "currentTime";
+const padID = [ "Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+const samples = [
+	"tom-l",
+	"tom-h",
+	"crash",
+	"opn-hat",
+	"snare",
+	"rim",
+	"closed-hat",
+	"kick-h",
+	"kick-s",
+	]
 
 $(document).ready(function() {
 	console.log("Doc ready");
@@ -33,18 +45,51 @@ $(document).ready(function() {
 
 
 function buildPads() {
-	const container = $("#pads");
-	for (r = 0; r<3; r++) {
-		container.append("<div class=\"pad-row\"><div>");
-		// remove the inner div (can't figure out why it adds it)
-		$(".pad-row:last div").remove();
-		
+
+	let padsHTML = ``;
+	let index = 0;
+	
+	for (let r = 0; r<3; r++) {
+		// open a div with class pad-row
+		padsHTML += `<div class="pad-row">`;
+		for (let c=0; c<3; c++) {
+			// open a div with class single-pad__container
+			padsHTML += `<div class="single-pad__container">`;
+
+			// add div class single-pad__top-margin
+			padsHTML += `<div class="single-pad__top-margin"></div>`;
+
+			// open div class="drum-pad"
+			// with id that describes the audio clip
+			padsHTML += `<div class="drum-pad" id="${index}" >`;
+
+			// with an inner text that corresponds to one of the following keys on the keyboard: qweasdzxc
+			padsHTML += `${padID[index]}`;
 
 
-		// continue;
-		for (c=0; c<3; c++) {
-			$(".pad-row:last").append("<div class=\"single-pad__container\"><div>");
-			$(".pad-row:last .single-pad__container:last div").remove();
+			// inside drum-pad open an audio tag which:
+			// has a src attribute pointing to an audio clip
+			// a class name of clip
+			// and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.).
+			padsHTML += `<audio src="${samples[index]}.wav" preload="audio" class="clip" id=${padID[index]}></audio>`
+
+
+
+			// close drum-pad div
+			padsHTML += `</div>`;
+			// close single-pad__container
+			padsHTML += `</div>`;
+
+
+			// incrment index
+			index++;
 		}
+
+		// close the pad_row div
+		padsHTML += `</div>`;
 	}
+
+	console.log(padsHTML);
+	// set #pads innerHTML
+	$("#pads").html(padsHTML); // do you need to do this?
 }
